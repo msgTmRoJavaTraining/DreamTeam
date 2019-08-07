669336f0f79f95e5404a03b3daf7eccb8f5c4b95
Razvan Mihaescu <razvan.mihaescu@student.upt.ro>
2019-08-07 15:29:38 +0300

    Implmented a rough login backend.

--- src/main/java/group/msg/jsf_ejb/DatabaseEJB.java
+++ src/main/java/group/msg/jsf_ejb/DatabaseEJB.java
@@ -2,20 +2,32 @@ package group.msg.jsf_ejb;
 
 import group.msg.entities.User;
 
-import javax.annotation.PostConstruct;
-import javax.ejb.Startup;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import java.io.Serializable;
 
 @Stateless
-@Startup
-public class DatabaseEJB  {
-    @PersistenceContext(name="java.training")
+public class DatabaseEJB implements Serializable {
+
+    @PersistenceContext(name = "java.training")
     EntityManager entityManager;
-    @PostConstruct
-    public void onStartUp()
-    {
+
+    public boolean login(String username, String password) {
+        String select = "SELECT u FROM User u WHERE u.username=:username and u.password=:password";
+
+        Query query = entityManager.createQuery(select);
+        query.setParameter("username", username);
+        query.setParameter("password", password);
+
+        try{
+            User ua = (User) query.getSingleResult();
+        }catch (NoResultException e)
+        {
+            return false;
+        }
+       return true;
     }
 }
