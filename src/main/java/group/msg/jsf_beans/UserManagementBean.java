7947d16c0e71eaa258c206e0f3c0a152434ca33f
bencehach <bencehach@gmail.com>
2019-08-09 12:11:05 +0300

    Added notifications for invalid credentials

--- src/main/java/group/msg/jsf_beans/UserManagementBean.java
+++ src/main/java/group/msg/jsf_beans/UserManagementBean.java
@@ -10,6 +10,8 @@ import lombok.Setter;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
