53d8261bd4bae65b7567481f6621aa89ff28eede
Razvan Mihaescu <razvan.mihaescu@student.upt.ro>
2019-08-12 12:19:02 +0300

    Clearing fields for user and bug adding.

--- src/main/java/group/msg/jsf_beans/FileUploadView.java
+++ src/main/java/group/msg/jsf_beans/FileUploadView.java
@@ -4,19 +4,21 @@ import lombok.Data;
 import org.primefaces.event.FileUploadEvent;
 import org.primefaces.model.UploadedFile;
 
-import javax.enterprise.context.SessionScoped;
-import javax.faces.application.FacesMessage;
-import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 import java.io.Serializable;
 
 @Data
 @Named
-@SessionScoped
+@ViewScoped
 public class FileUploadView implements Serializable {
 
     private UploadedFile file;
 
+    public void clearFile()
+    {
+        file=null;
+    }
 
     public void handleFileUpload(FileUploadEvent event) {
         file = event.getFile();
